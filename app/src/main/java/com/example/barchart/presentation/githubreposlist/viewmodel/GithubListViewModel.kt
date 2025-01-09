5beaca3c4c6233b422722cc3372fc2fd6836b37d
ramesh <rameshgetit@gmail.com>
2025-01-09 19:56:45 +0530

    updated ViewModelStateFlow in view model

--- app/src/main/java/com/example/barchart/presentation/githubreposlist/viewmodel/GithubListViewModel.kt
+++ app/src/main/java/com/example/barchart/presentation/githubreposlist/viewmodel/GithubListViewModel.kt
@@ -1,33 +1,30 @@
 package com.example.barchart.presentation.githubreposlist.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.barchart.domain.githubreposlist.repository.GithubListRepository
-import com.example.barchart.presentation.githubreposlist.mapper.mapDomainToPresentationReposList
 import com.example.barchart.presentation.githubreposlist.model.GithubListUIModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
+//hilt
 @HiltViewModel
 class GithubListViewModel @Inject constructor(
     val githubListRepository: GithubListRepository
 ) : ViewModel() {
 
-    private var _reposList: MutableLiveData<List<GithubListUIModel>> = MutableLiveData()
-    val reposList: LiveData<List<GithubListUIModel>> = _reposList
+    //hot flow
+    internal var uiState: StateFlow<HomeUiState> = MutableStateFlow(HomeUiState(emptyList()))
 
-    fun fetchReposList() {
+    fun fetchReposList() =
         viewModelScope.launch {
-            val result = githubListRepository.fetchReposList()
-            //mapping to ui model
-            _reposList.value = mapDomainToPresentationReposList(result)
-
-            //to log
-            println("final result in view model: $reposList")
+            githubListRepository.fetchReposList().collectLatest { println("final result in view model: ${it}") }
         }
-    }
 }
+
+data class HomeUiState(val data: List<GithubListUIModel>)
