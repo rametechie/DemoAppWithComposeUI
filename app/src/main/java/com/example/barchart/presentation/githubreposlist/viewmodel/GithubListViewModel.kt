0edeb395e82dbe77c020e4fe45ba1e218c011e7b
ramesh <rameshgetit@gmail.com>
2025-01-11 06:56:01 +0530

    updated ViewModelStateFlow in view model

--- app/src/main/java/com/example/barchart/presentation/githubreposlist/viewmodel/GithubListViewModel.kt
+++ app/src/main/java/com/example/barchart/presentation/githubreposlist/viewmodel/GithubListViewModel.kt
@@ -8,6 +8,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
